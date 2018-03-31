c03686f2f09ea6631860c70d5a7bc0a54cbd0318
Denis Chagin <chaginden@gmail.com>
2018-03-31 20:52:23 +0300

    pyperboard: Add Table of content feature
    - Add tocbot.js script
    - Add `toc` markdown extension
    - Add `toc.css` stylesheet

--- pyperboard/converter/converter.py
+++ pyperboard/converter/converter.py
@@ -2,5 +2,5 @@ import markdown
 
 
 def convert_md(md_text: str) -> str:
-    html = markdown.markdown(md_text, extensions=['extra'])
+    html = markdown.markdown(md_text, extensions=['extra', 'toc'])
     return html
