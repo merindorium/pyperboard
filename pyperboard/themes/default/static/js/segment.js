a0c7ad4f9a8375fcd70805c052fc4fd65916a669
Denis Chagin <denis.chagin@emlid.com>
2018-05-22 15:20:10 +0300

    pyperboard: themes: Add styling and front-end functionality for... ... Segment extension
    - Add external styling file for segments
    - Add JS code for segment functionality (e.g. show, hide segments)
    - Update index page

---
+++ pyperboard/themes/default/static/js/segment.js
@@ -0,0 +1,63 @@
+document.body.addEventListener("load", initSegmentMenus(), false);
+
+function initSegmentMenus() {
+
+    var segmentMenus = document.getElementsByClassName("segment-menu");
+
+    for (var i = 0; i < segmentMenus.length; i++) {
+
+        var segmentMenu = segmentMenus[i];
+
+        var segmentGroup = segmentMenu.dataset.group;
+        var segmentMenuItems = document.querySelectorAll('[id*="' + segmentGroup + '__"]');
+
+        segmentMenuItems.forEach(function (element, index) {
+            var itemSegment = element.dataset.segment;
+            var menuItem = createSegmentMenuItem(itemSegment, segmentGroup);
+
+            segmentMenu.appendChild(menuItem);
+
+            if (index === 0) {
+                menuItem.click();
+            }
+        });
+
+    }
+}
+
+function createSegmentMenuItem(segmentName, segmentGroup) {
+    var button = document.createElement('span');
+    button.classList.add('segment-menu-item');
+    button.innerText = segmentName.charAt(0).toUpperCase() + segmentName.slice(1);
+    button.dataset.segment = segmentName;
+
+    button.onclick = function (event) {
+        var parentMenu = event.target.parentNode;
+        clearSegmentButtonsState(parentMenu);
+        event.target.classList.add("is-selected");
+        showSegment(segmentName, segmentGroup);
+    };
+
+    return button;
+}
+
+
+function clearSegmentButtonsState(parentMenu) {
+
+    var segmentMenuItems = parentMenu.querySelectorAll('.segment-menu-item');
+
+    segmentMenuItems.forEach(function (element) {
+        element.classList.remove("is-selected");
+    });
+}
+
+
+function showSegment(segmentName, segmentGroup) {
+    var segments = document.querySelectorAll('[id*="' + segmentGroup + '__"]');
+
+    segments.forEach(function (element) {
+        element.classList.remove("active");
+    });
+
+    document.getElementById(segmentGroup + '__' + segmentName).classList.add("active");
+}
