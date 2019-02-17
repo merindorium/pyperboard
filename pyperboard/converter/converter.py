225d627826c2c7bf6ea866af4ed8599879700bb2
Denis Chagin <chaginden@gmail.com>
2019-02-18 00:09:36 +0300

    converter: Move `md` variable to `convert_md` function

--- pyperboard/converter/converter.py
+++ pyperboard/converter/converter.py
@@ -2,13 +2,12 @@ import markdown
 
 from pyperboard.converter import extensions
 
-md = markdown.Markdown(extensions=['extra',
-                                   'toc',
-                                   'codehilite',
-                                   extensions.RestApiExtension(),
-                                   extensions.SegmentExtension()])
-
 
 def convert_md(md_text: str) -> str:
+    md = markdown.Markdown(extensions=['extra',
+                                       'toc',
+                                       'codehilite',
+                                       extensions.RestApiExtension(),
+                                       extensions.SegmentExtension()])
     html = md.convert(md_text)
     return html
