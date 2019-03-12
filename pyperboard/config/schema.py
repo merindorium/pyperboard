99a3ba93978db95b58db13f09ca15661bd48d599
Denis Chagin <chaginden@gmail.com>
2019-03-12 19:23:11 +0300

    pyperboard: config: Add Config validation schema
    - Add File validation field
    - Add ConfigSchema validation schema

---
+++ pyperboard/config/schema.py
@@ -0,0 +1,22 @@
+import os
+from typing import Any
+
+from marshmallow import EXCLUDE, Schema, fields
+
+
+class File(fields.String):
+    default_error_messages = {"invalid": "{file_name} does not exist"}
+
+    def _validate(self, value: Any) -> None:
+        working_dir = os.getcwd()
+        file_abs_path = os.path.join(working_dir, value)
+
+        if not os.path.isfile(file_abs_path):
+            self.fail("invalid", file_name=value)
+
+
+class ConfigSchema(Schema):
+    pages = fields.List(File, required=True)
+
+    class Meta:
+        unknown = EXCLUDE
