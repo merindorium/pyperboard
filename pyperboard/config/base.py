528feea61a00548df8fda4e81789115383646118
Denis Chagin <chaginden@gmail.com>
2019-03-13 21:39:06 +0300

    pyperboard: config: Update Config class
    - Add extensions names list

--- pyperboard/config/base.py
+++ pyperboard/config/base.py
@@ -4,6 +4,7 @@ from typing import List
 class Config:
     def __init__(self) -> None:
         self.pages: List[str] = []
+        self.extensions: List[str] = []
 
     def update(self, config: dict) -> None:
         for k, v in config.items():
